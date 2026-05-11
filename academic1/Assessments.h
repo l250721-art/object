e1b38f0a85a1bc281a16c3d4d38ced20eb6035e7
l250721-art <l250721@lhr.nu.edu.pk>
2026-05-11 11:19:40 +0500

    updated assessments.h

--- academic1/Assessments.h
+++ academic1/Assessments.h
@@ -1,1 +1,29 @@
 #pragma once
+#include "Assessment.h"
+
+
+class Exam : public Assessment {
+public:
+    Exam();
+    Exam(double raw, double max, double weight);
+
+    double getWeightedScore() const override;
+    void   display()          const override;
+};
+
+class Quiz : public Assessment {
+public:
+    Quiz();
+    Quiz(double raw, double max, double weight);
+
+    double getWeightedScore() const override;
+    void   display()          const override;
+};
+class Assignment : public Assessment {
+public:
+    Assignment();
+    Assignment(double raw, double max, double weight);
+
+    double getWeightedScore() const override;
+    void   display()          const override;
+};
